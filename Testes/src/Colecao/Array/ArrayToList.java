1dc5caae76a775ae6eca1582cc0e8a81bc31d666
Gabriel <Gabriel@DESKTOP-HPQK0N9>
2019-12-12 19:21:40 -0300

    Inicio Resolução prova poo

--- Testes/src/Colecao/Array/ArrayToList.java
+++ Testes/src/Colecao/Array/ArrayToList.java
@@ -17,3 +17,6 @@ public class ArrayToList {
 			System.out.println(cor);
 	}
 }
+
+
+
---
+++ Testes/src/poo/slide2/encapsulamento/AbstracaoFuncao.java
@@ -0,0 +1,13 @@
+package poo.slide2.encapsulamento;
+
+public class AbstracaoFuncao {
+
+	public static void main(String[] args) {
+		System.out.println(soma(2, 3));
+	}
+	public static int soma(int x, int y) {
+		int a=x, b=y, c;
+		c = a + b;
+		return c;
+	}
+}
